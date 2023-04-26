058f6a51f3996e894d3bd708f4fbb514e7cdd14b
Levi Lee <dlrbqls5126@gmail.com>
2023-04-25 18:19:37 -0600

    name and grade

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -9,10 +9,6 @@ namespace game
             Console.WriteLine("Hello Sandbox World!!!");
             Console.WriteLine("This is in C#.");
 
-            Console.WriteLine("What is your favorite colore? ");
-            String color = Console.ReadLine();
-            Console.Write(color);
-            
         }
     }
 }
