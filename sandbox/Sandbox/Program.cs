19f9f3b7995daa7b5b2997f3fd69042b4a8c8bd2
Levi Lee <dlrbqls5126@gmail.com>
2023-05-03 13:08:30 -0600

    learning 2

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,9 +6,52 @@ namespace game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Sandbox World!!!");
-            Console.WriteLine("This is in C#.");
+            // object
+            // instance
+            var Layne= new Person("Layne", "Moseley");
+            var David= new Person("David", "Hasselhoff");
 
+            Layne.Talk();
+            David.Talk();
         }
     }
+}
+
+// classification (keyword)
+class Person {
+
+    // attributes
+    // properties
+    string firstName;
+    string lastName;
+
+    //  constructor
+    public Person(string fn, string ln) {
+        firstName = fn;
+        lastName = ln;
+    }
+
+    // behaviors
+    // method
+    public void Breathe() {
+        Console.WriteLine("Breating");
+    }
+
+    public void Walk() {
+        Console.WriteLine("Walking");
+    }
+
+    public void Talk() {
+        Console.WriteLine($"Hi! my name is {firstName} {lastName}");
+    }
+
+    public string FullName() {
+        // return $"{firstName} {lastName}";
+        
+        // string full = String.Concat(firstName, lastName);
+        // return full;
+
+        return firstName + " " + lastName;
+    }
+
 }
