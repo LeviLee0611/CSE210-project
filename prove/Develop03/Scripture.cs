190681231a0823fffdc9ba4fe5009124b6c4b893
Levi Lee <dlrbqls5126@gmail.com>
2023-07-15 23:57:26 -0600

    Change

--- prove/Develop03/Scripture.cs
+++
@@ -1,9 +0,0 @@
-class Scripture {
-    private string _scripturesText;
-
-    private string _scripturesList;
-
-    private string _reference;
-    public List<Scripture> _scriptures = new List<Scripture>();
-
-}
